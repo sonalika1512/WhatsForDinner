d60aea4452f1256ceb0340ebd5ce93022ded8b54
sonalika singh <123459406+sonalika1512@users.noreply.github.com>
2024-04-29 20:17:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import Navbar from  './Components/Navbar/Navbar.js' ;
 import Home from  './Components/Home/Home.js' ;
-import WeeklyMeal from  './Components/WeeklyMeal/WeeklyMeal.js' ;
+import WeeklyMeal from  './Components/weeklyMeal/WeeklyMeal.js' ;
 import Footer from  './Components/Footer/Footer.js' ;
 import { DisplaySavedRecipe, SearchRestaurants } from './Components/Restaurants/Restaurants.js'; // Import the components correctly
 
@@ -18,6 +18,7 @@ function App() {
     <DisplaySavedRecipe />
     <SearchRestaurants/>
     <Footer/>
+
 
     </>
 
